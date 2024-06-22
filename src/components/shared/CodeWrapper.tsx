970c755dc2f2a8d58ee71799a5c331f7f18b8849
Shrinath Nayak <shrinathnayak07@gmail.com>
2024-06-22 22:23:31 +0530

    Code highlight fix

--- src/components/shared/CodeWrapper.tsx
+++ src/components/shared/CodeWrapper.tsx
@@ -1,35 +1,19 @@
 "use client";
 
 import { ShikiProvider } from "@mantinex/shiki";
-import React, { useState, useEffect } from "react";
 
-const loadShiki = async () => {
+async function loadShiki() {
   const { getHighlighter } = await import("shiki");
   const shiki = await getHighlighter({
     langs: ["tsx", "scss", "html", "bash", "json"],
     themes: [],
   });
+
   return shiki;
-};
+}
 
-// ShikiLoader component to handle async loading
 const ShikiLoader = ({ children }: { children: React.ReactNode }) => {
-  const [highlighter, setHighlighter] = useState(null);
-
-  useEffect(() => {
-    const fetchShiki = async () => {
-      try {
-        const highlighter = await loadShiki();
-        setHighlighter(highlighter);
-      } catch (error) {
-        console.error("Error loading Shiki highlighter:", error);
-      }
-    };
-
-    fetchShiki();
-  }, []);
-
-  return <ShikiProvider loadShiki={loadShiki}>{children}</ShikiProvider>;
+  return <ShikiProvider loadShiki={loadShiki as any}>{children}</ShikiProvider>;
 };
 
 export default ShikiLoader;
