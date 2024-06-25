1cc6770cfc9f459b494824337ab52675569be1a1
Shrinath <shrinath.nayak@cashfree.com>
2024-06-25 12:45:13 +0530

    GA integration

--- src/observability/index.tsx
+++ src/observability/index.tsx
@@ -1,5 +1,11 @@
+import GoogleAnalytics from "./GoogleAnalytics";
 import MicrosoftClarity from "./MicrosoftClarity";
 
-const Metrics = () => <MicrosoftClarity />;
+const Metrics = () => (
+  <>
+    <MicrosoftClarity />
+    <GoogleAnalytics />
+  </>
+);
 
 export default Metrics;
