c3a8796469e5b928419cd02596df40eb36ce00c1
Shrinath Nayak <shrinathnayak07@gmail.com>
2024-06-29 04:37:08 +0530

    fix: disable analytics in development mode

--- src/observability/index.tsx
+++ src/observability/index.tsx
@@ -1,13 +1,19 @@
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import GoogleAnalytics from "./GoogleAnalytics";
 import MicrosoftClarity from "./MicrosoftClarity";
 
-const Metrics = () => (
-  <>
-    <Analytics />
-    <MicrosoftClarity />
-    <GoogleAnalytics />
-  </>
-);
+const Metrics = () => {
+  if (process.env.NODE_ENV !== "production") {
+    return null;
+  }
+
+  return (
+    <>
+      <Analytics />
+      <MicrosoftClarity />
+      <GoogleAnalytics />
+    </>
+  );
+};
 
 export default Metrics;
