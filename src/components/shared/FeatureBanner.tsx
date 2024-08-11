cce2f6598abc67a20e5315d4aa0a07829a866c31
Shrinath Nayak <shrinathnayak07@gmail.com>
2024-08-11 21:49:01 +0530

    fix: updated introducing banner

--- src/components/shared/FeatureBanner.tsx
+++ src/components/shared/FeatureBanner.tsx
@@ -4,32 +4,33 @@ import { Flex, Text, Badge, Affix } from "@mantine/core";
 const FeatureBanner = () => {
   return (
     <Affix position={{ top: 0, right: 0, left: 0 }}>
-    <Flex
-      p={12}
-      gap={10}
-      bg="dark.9"
-      align="center"
-      justify="center"
-      direction={{
-        base: "column",
-        sm: "row",
-      }}
-      style={{
-        boxShadow: "var(--mantine-shadow-md)",
-      }}
-    >
-      <Badge
-        color="red.8"
-        size="md"
-        display={{ base: "none", sm: "flex" }}
-        radius="sm"
+      <Flex
+        p={12}
+        gap={10}
+        bg="dark.9"
+        align="center"
+        justify="center"
+        direction={{
+          base: "column",
+          sm: "row",
+        }}
+        style={{
+          boxShadow: "var(--mantine-shadow-md)",
+        }}
       >
-        Feature
-      </Badge>
-      <Text fz="xs" ta="center">
-        Now you can view the package score, which includes metrics for Supply Chain Risk, Quality, Maintenance, Vulnerability, and License.
-      </Text>
-    </Flex>
+        <Badge
+          color="red.8"
+          size="md"
+          display={{ base: "none", sm: "flex" }}
+          radius="sm"
+        >
+          Feature
+        </Badge>
+        <Text fz="xs" ta="center">
+          Now you can view the package score, which includes metrics for Supply
+          Chain Risk, Quality, Maintenance, Vulnerability, and License.
+        </Text>
+      </Flex>
     </Affix>
   );
 };
