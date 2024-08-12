ad9d01978b4b9c093fc6376201fb681e3c8ba854
Shrinath Nayak <shrinathnayak07@gmail.com>
2024-08-12 23:50:12 +0530

    fix: sitemap url fix

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,10 +2,13 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      }
+    ],
+    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap2.xml`,
+    host: process.env.NEXT_PUBLIC_SITE_URL,
   };
 }
