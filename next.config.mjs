c0fde70cc4062e5ca98f517cdef6dc0eaccbb74f
Shrinath Nayak <shrinathnayak07@gmail.com>
2024-08-28 22:20:51 +0530

    fix: removed webpack from next config file

--- next.config.mjs
+++ next.config.mjs
@@ -38,16 +38,8 @@ const nextConfig = {
       },
     ],
   },
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.optimization.splitChunks = {
-        ...config.optimization.splitChunks,
-        maxSize: 20000,
-      };
-    }
-    return config;
-  },
 };
+
 const withMDX = createMDX({
   options: {
     remarkPlugins: [remarkGfm],
