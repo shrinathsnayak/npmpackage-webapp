8454b6e9041a776f83af5f2ae6f786ed9d39826f
Shrinath Nayak <shrinathnayak07@gmail.com>
2024-10-11 03:11:41 +0530

    fix: route change progressbar

--- src/components/shared/NavigationProgressBar.tsx
+++ src/components/shared/NavigationProgressBar.tsx
@@ -1,18 +1,32 @@
 "use client";
 
 import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { usePathname } from "next/navigation";
 import { NavigationProgress, nprogress } from "@mantine/nprogress";
 
 export const NavigationProgressBar = () => {
+  const router = useRouter();
   const pathname = usePathname();
 
   useEffect(() => {
-    nprogress.complete();
-    return () => {
-      nprogress.start();
+    const handleStart = (url: any) =>
+      url !== router.asPath && nprogress.start();
+
+    const handleComplete = () => {
+      nprogress.complete();
     };
-  }, [pathname]);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router.events, router.asPath, pathname]);
 
   return <NavigationProgress color="red.8" size={3} />;
 };
