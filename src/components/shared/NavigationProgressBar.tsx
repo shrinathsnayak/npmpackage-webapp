b4e7dd4e3451d4344575d7992dbd40ca1599e4dd
Shrinath Nayak <shrinathnayak07@gmail.com>
2024-10-13 20:59:24 +0530

    fix: navigation progress fix

--- src/components/shared/NavigationProgressBar.tsx
+++ src/components/shared/NavigationProgressBar.tsx
@@ -1,32 +1,18 @@
-"use client";
+'use client'
 
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import { usePathname } from "next/navigation";
-import { NavigationProgress, nprogress } from "@mantine/nprogress";
+import { useEffect } from 'react'
+import { usePathname } from 'next/navigation'
+import { NavigationProgress, nprogress } from '@mantine/nprogress'
 
 export const NavigationProgressBar = () => {
-  const router = useRouter();
-  const pathname = usePathname();
+  const pathname = usePathname()
 
   useEffect(() => {
-    const handleStart = (url: any) =>
-      url !== router.asPath && nprogress.start();
-
-    const handleComplete = () => {
-      nprogress.complete();
-    };
-
-    router.events.on("routeChangeStart", handleStart);
-    router.events.on("routeChangeComplete", handleComplete);
-    router.events.on("routeChangeError", handleComplete);
-
+    nprogress.complete()
     return () => {
-      router.events.off("routeChangeStart", handleStart);
-      router.events.off("routeChangeComplete", handleComplete);
-      router.events.off("routeChangeError", handleComplete);
-    };
-  }, [router.events, router.asPath, pathname]);
+      nprogress.start()
+    }
+  }, [pathname])
 
-  return <NavigationProgress color="red.8" size={3} />;
-};
+  return <NavigationProgress color='red.8' size={10} />
+}
