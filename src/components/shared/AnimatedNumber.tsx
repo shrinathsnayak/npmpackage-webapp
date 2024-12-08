62d876cf6c752f0c022f3623fa838eb94ad2f0b4
Shrinath Nayak <shrinathnayak07@gmail.com>
2024-12-08 19:52:48 +0530

    refactor: Comment out NumberFlow integration in AnimatedNumber component for simplified rendering

--- src/components/shared/AnimatedNumber.tsx
+++ src/components/shared/AnimatedNumber.tsx
@@ -1,17 +1,18 @@
 "use client";
 
 import { useMemo } from "react";
-import NumberFlow, { useCanAnimate } from "@number-flow/react";
+// import NumberFlow, { useCanAnimate } from "@number-flow/react";
 import { NumberFormatter } from "@mantine/core";
 
 const AnimatedNumber = ({ value }: { value: string | number }) => {
-  const canAnimate = useCanAnimate();
+  // const canAnimate = useCanAnimate();
   const parsedValue = useMemo(() => (value ? Number(value) : 0), [value]);
-  return canAnimate ? (
-    <NumberFlow value={parsedValue} isolate={true} locales="en-US" />
-  ) : (
-    <NumberFormatter value={parsedValue} thousandSeparator />
-  );
+  return <NumberFormatter value={parsedValue} thousandSeparator />;
+  // return canAnimate ? (
+  //   <NumberFlow value={parsedValue} isolate={true} locales="en-US" />
+  // ) : (
+  //   <NumberFormatter value={parsedValue} thousandSeparator />
+  // );
 };
 
 export default AnimatedNumber;
