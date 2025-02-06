f764faad42e4a9bc282d594f3807ddbd22b0460a
Shrinath Nayak <shrinathnayak07@gmail.com>
2025-02-06 19:34:14 +0530

    feat: Implement pagination for fetching popular packages in chunks from Supabase

--- src/services/supbase.ts
+++ src/services/supbase.ts
@@ -75,25 +75,47 @@ export async function getPopularPackages() {
 }
 
 /**
- * The function `getAllPopularPackages` retrieves popular package IDs from a Supabase table in
- * descending order of count.
- * @returns The `getAllPopularPackages` function is returning the data fetched from the "popular" table
- * in the Supabase database. The data consists of the package IDs of popular packages, ordered by count
- * in descending order. If there is an error during the fetching process, the function will log an
- * error message and return `false`.
+ * This TypeScript function retrieves all popular packages from a database in chunks of 1000 records.
+ * @returns The `getAllPopularPackages` function returns an array of objects containing `package_id`
+ * and `updated_at` properties for popular packages fetched from a Supabase database.
  */
 export async function getAllPopularPackages() {
-  const { data, error } = await supabase
-    .from("popular")
-    .select("package_id, updated_at")
-    .order("count", { ascending: false })
-    .limit(5000);
+  let allData: any = [];
+  let offset = 0;
+  const chunkSize = 1000;
 
-  if (error) {
-    console.error("Error fetching popular packages:", error.message);
+  const { count, error: countError } = await supabase
+    .from("popular")
+    .select("package_id", { count: "exact" });
+
+  if (countError) {
+    console.error("Error fetching total count:", countError.message);
     return false;
   }
-  return data;
+
+  if (count === 0) {
+    return allData;
+  }
+
+  while (count && offset < count) {
+    const { data, error } = await supabase
+      .from("popular")
+      .select("package_id, updated_at")
+      .order("count", { ascending: false })
+      .range(offset, offset + chunkSize - 1);
+
+    if (error) {
+      console.error("Error fetching popular packages:", error.message);
+      return false;
+    }
+
+    if (data.length === 0) break;
+
+    allData = [...allData, ...data];
+    offset += chunkSize;
+  }
+
+  return allData;
 }
 
 /**
