647b52324c9026881ef4ad0f73072b5ed9278852
Shrinath Nayak <shrinathnayak07@gmail.com>
2025-04-16 00:38:11 +0530

    fix: Update ReadMe component styling for improved layout and appearance

--- src/components/packages/Tabs/ReadMe.tsx
+++ src/components/packages/Tabs/ReadMe.tsx
@@ -11,7 +11,7 @@ const ReadMe = ({ data, gitHub }: any) => {
 
   return (
     <Suspense fallback={<>loading...</>}>
-      <Paper p="lg" radius="md" bg="dark.7" shadow="sm" c="white" withBorder>
+      <Paper p={0} bg="dark.9" c="white">
         <MDX content={data} owner={owner} name={name} branch={defaultBranch} />
       </Paper>
     </Suspense>
